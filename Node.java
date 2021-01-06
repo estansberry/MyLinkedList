517a9b45198798e1932d2c7b84bff1c1d6e6636a
Elizabeth Stansberry <estansberry20@stuy.edu>
2021-01-06 11:10:58 -0700

    Set up

--- Node.java
+++ Node.java
@@ -1,6 +1,32 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
+ public Node(String value){
+
+ }
  private String data;
  private Node next,prev;
  //write get/set methods for all three instance variables.
+
+ public String getData(int index){
+   return(data);
+ }
+
+ public String getNext(int index){
+
+ }
+
+ public String getPrev(int index){
+
+ }
+
+ public void setData(int index, String value){
+
+ }
+
+ public void setNext(int index, String value){
+
+ }
+
+ public void setPrev(int index, String value){
+
+ }
 }
